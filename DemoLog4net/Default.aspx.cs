d9a9d8577ebb44d8fe28ff247bd018d9da4db2b4
Sreelekha Bolloju <Sreelekha_Bolloju@epam.com>
2020-02-05 14:41:42 +0530

    Add project files.

---
+++ DemoLog4net/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace DemoLog4net
+{
+    
+
+    public partial class _Default : Page
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(Server.MapPath("~/log4net.config")));
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch(DivideByZeroException ex)
+            {
+                log.Error(ex);
+            }
+        }
+    }
+}
---
+++ DemoLog4net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoLog4net.Global" Language="C#" %>
